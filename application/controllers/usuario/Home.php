25984f30b2b272799977b66f308f62a363da1a19
Unknown <edsouzaguedes@gmail.com>
2017-11-16 09:53:38 -0300

    /RFGB00-Login-provissorio: Auto stash before rebase of "origin/develop"

---
+++ application/controllers/usuario/Home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+
+
+	public function login($papel){
+	
+		// session_start inicia a sessão
+		session_start();
+		$_SESSION['papel'] = $papel;		
+		//redirect('home/', 'refresh');
+		$this->twig->display('home');
+	}
+
+
+}
+
